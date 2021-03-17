3c442f36c214dee7de96403978bae0ebc8ca60d7
Kei Shirakizawa <ks@blackbird.online>
2021-03-17 20:52:57 +0100

    Make javadoc compile
    Still sees a lot of warings.

--- src/main/java/net/codekata/cloudkeytool/Utils.java
+++ src/main/java/net/codekata/cloudkeytool/Utils.java
@@ -6,18 +6,21 @@ import io.atlassian.fugue.Option;
 import io.atlassian.fugue.Try;
 import io.atlassian.fugue.Unit;
 
+/** Utility methods for fugue types. */
 public final class Utils {
-  /** A sensible default for unit type provided by fugue */
+  /** A sensible default for unit type provided by fugue. */
   public static Unit unit() {
     return Unit.valueOf("VALUE");
   }
 
-  /** Transforms the Option<A> into a Try<A>, mapping None to NoSuchElementException. */
+  /**
+   * Transforms the {@code Option<A>} into a {@code Try<A>}, mapping None to NoSuchElementException.
+   */
   public static <A> Try<A> fromOption(Option<A> opt) {
     return now(opt::get);
   }
 
-  /** Lift any exceptions to unchecked one. */
+  /** Lifts any exceptions to unchecked one. */
   public static <T> T throwRuntime(Exception cause) {
     throw new RuntimeException(cause);
   }
